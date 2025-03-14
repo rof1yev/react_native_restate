5db18bafe9f45e3db791cee10189c48b6621f021
rof1yev <rofiyevdilshod@gmail.com>
2025-03-14 17:59:13 +0500

    Implement Home screen UI

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,83 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { Link } from "expo-router";
+import images from "@/constants/images";
+import { useGlobalContext } from "@/hooks/global-provider";
+import icons from "@/constants/icons";
+import Search from "@/components/Search";
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 
 export default function Index() {
+  const { user } = useGlobalContext();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-2xl text-blue-500 mb-4">Welcome ReState!</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView>
+        <View className="px-5">
+          <View className="flex flex-row items-center justify-between mt-5">
+            <View className="flex flex-row items-center">
+              <Image source={images.avatar} className="size-12 rounded-full" />
+              <View className="flex flex-col items-start ml-2 justify-center">
+                <Text className="text-xs font-rubik text-black-100">
+                  Good Morning
+                </Text>
+                <Text className="text-base font-rubik-medium text-black-300">
+                  {user?.name}
+                </Text>
+              </View>
+            </View>
+            <Image source={icons.bell} className="size-6" />
+          </View>
+
+          <Search />
+
+          <View className="my-5">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Featured
+              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  Sell All
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View className="flex flex-row gap-5 mt-5">
+                <FeaturedCard />
+                <FeaturedCard />
+                <FeaturedCard />
+              </View>
+            </ScrollView>
+          </View>
+
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Our Recommendation
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                Sell All
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <Filters />
+
+          <View className="flex flex-row gap-5 mt-5">
+            <Card />
+            <Card />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
