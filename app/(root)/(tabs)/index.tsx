abddcfcb34237606c142918ed46addda02a4d91d
rof1yev <rofiyevdilshod@gmail.com>
2025-03-15 16:22:21 +0500

    Display data

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -5,30 +5,68 @@ import {
   Image,
   TouchableOpacity,
   FlatList,
-  Button,
+  ActivityIndicator,
 } from "react-native";
 import { useGlobalContext } from "@/hooks/global-provider";
 import icons from "@/constants/icons";
 import Search from "@/components/Search";
 import { Card, FeaturedCard } from "@/components/Cards";
 import Filters from "@/components/Filters";
-import seed from "@/lib/seed";
+import { router, useLocalSearchParams } from "expo-router";
+import { useAppwrite } from "@/hooks/useAppwrite";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
+import { useEffect } from "react";
+import NoResults from "@/components/NoResults";
 
 export default function Index() {
   const { user } = useGlobalContext();
+  const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+  const { data: latestProperties, loading: latestPropertiesLoading } =
+    useAppwrite({
+      fn: getLatestProperties,
+      params,
+    });
+
+  const {
+    data: properties,
+    loading,
+    refetch,
+  } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6,
+    },
+    skip: true,
+  });
+
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`);
+
+  useEffect(() => {
+    refetch({ filter: params.filter!, query: params.query!, limit: 6 });
+  }, [params.filter, params.query]);
 
   return (
     <SafeAreaView className="h-full bg-white">
-      <Button title="Send" onPress={seed} />
-
       <FlatList
-        data={[1, 2, 3, 4]}
-        renderItem={({ item }) => <Card />}
-        keyExtractor={(item) => item.toString()}
+        data={properties}
+        renderItem={({ item }) => (
+          <Card item={item} onPress={() => handleCardPress(item.$id)} />
+        )}
+        keyExtractor={(item) => item.$id}
         numColumns={2}
         contentContainerClassName="pb-32"
         columnWrapperClassName="flex gap-5 px-5"
         showsHorizontalScrollIndicator={false}
+        ListEmptyComponent={
+          loading ? (
+            <ActivityIndicator size="large" className="text-primary-300 mt-5" />
+          ) : (
+            <NoResults />
+          )
+        }
         ListHeaderComponent={
           <View className="px-5">
             <View className="flex flex-row items-center justify-between mt-5">
@@ -63,15 +101,31 @@ export default function Index() {
                 </TouchableOpacity>
               </View>
 
-              <FlatList
-                data={[5, 6, 7]}
-                renderItem={({ item }) => <FeaturedCard />}
-                keyExtractor={(item) => item.toString()}
-                horizontal
-                bounces={false}
-                showsHorizontalScrollIndicator={false}
-                contentContainerClassName="flex gap-5 mt-5"
-              />
+              {latestPropertiesLoading ? (
+                <ActivityIndicator
+                  size="large"
+                  className="text-primary-300 mt-5"
+                />
+              ) : !latestProperties || latestProperties?.length === 0 ? (
+                <NoResults />
+              ) : (
+                <View>
+                  <FlatList
+                    data={latestProperties}
+                    renderItem={({ item }) => (
+                      <FeaturedCard
+                        item={item}
+                        onPress={() => handleCardPress(item.$id)}
+                      />
+                    )}
+                    keyExtractor={(item) => item.$id}
+                    horizontal
+                    bounces={false}
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerClassName="flex gap-5 mt-5"
+                  />
+                </View>
+              )}
             </View>
 
             <View className="flex flex-row items-center justify-between">
